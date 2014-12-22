4a9a38c78db28bbca8541f833e592de0e5c5aef0
Carl Goldberg <carlgoldberg1236@gmail.com>
2014-12-22 15:17:13 -0800

    Map initializes at current location using map directive

--- www/js/map/map.js
+++ www/js/map/map.js
@@ -2,15 +2,39 @@ angular.module('tsonga.map', [])
 
 .directive('leafletMap', function() {
 	
-
 	return {
-		link: function(scope, elmt, attrs) {
-			console.log('el', elmt); 
-			console.log('attr', attrs); 
-			var map = L.map(attrs.id).setView([51.505, -0.09], 13);
-			L.tileLayer('http://{s}.tiles.mapbox.com/v3/ceg1236.kiag8pom/{z}/{x}/{y}.png', {
-			    maxZoom: 18
+		restrict:'EA',
+		replace: true,
+		scope:{
+
+		},
+		template: '<div style="height: 400px"></div>',
+		link: function(scope, element, attrs) {
+
+			var map = L.map(element[0], {
+				dragging:true,
+				touchZoom:true,
+				tap:false,
+				zoom: 12, 
+				center: [37.741399, -122.43782],
+			});
+
+			function onLocationFound(e) {
+			    var radius = e.accuracy / 2;
+
+			    L.marker(e.latlng).addTo(map)
+			        .bindPopup("You are here!").openPopup();
+
+			    L.circle(e.latlng, radius).addTo(map);
+			}
+
+			map.on('locationfound', onLocationFound);
+
+			L.tileLayer('http://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+			    maxZoom: 18, 
+			    id: 'examples.map-i875mjb7'
 			}).addTo(map);
+			map.locate( {setView: true, zoom: 12 });
 		}
 	}
 });
