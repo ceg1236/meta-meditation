e2dbc61a578724b7520b9249acaf49777d7d5917
Carl Goldberg <carlgoldberg1236@gmail.com>
2015-01-08 12:49:37 -0800

    GET and PUT meditators/at routes basic set

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 var express = require("express"), 
 	app = express(), 
 	bodyParser = require('body-parser');
-app.set('view engine', 'ejs'); 
+// app.set('view engine', 'html'); 
 app.use( bodyParser.urlencoded({extended: false}) );
 
 app.get('/', function(req, res) {
-	res.render('index');
+	res.send('index');
 });
 
 app.post('/', function(req, res) {
 	console.log('request body: ', req.body); 
 	res.redirect('/');
 });
+
+app.get('/meditators/at/:lat/:lng', function(req, res) {
+	res.send('gettin meditators'); 
+});
+
+app.put('meditators/at/:lat/:lng', function(req, res) {
+	res.send('putting meditators'); 
+});
+
+
 app.listen(8003); 
