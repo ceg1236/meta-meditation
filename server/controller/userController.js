2f0c5584b797e02682a561c09d13ed26b3176f45
Jose Joaquin <jomerinog@gmail.com>
2015-02-02 16:03:18 -0800

    added user controller and tests

---
+++ server/controller/userController.js
@@ -0,0 +1,11 @@
+var User = require('../model/user');
+
+// finds or creates a user id
+var handShake = function (req, res) {
+
+
+};
+
+module.exports = {
+  handShake: handShake
+};
--- test/server/user.js
+++ test/server/user.js
@@ -32,7 +32,6 @@ describe('user', function () {
       expect(data.id).not.to.be.undefined();
       expect(data.name).to.equal('Carl');
 
-      console.log('saved');
       client.get('user:'+data.id, function(err, data){
 
         expect(JSON.parse(data).name).to.equal('Carl');
@@ -70,7 +69,8 @@ describe('user', function () {
           .post('/api/handshake')
           .send({name: "Jeff"})
           .expect(200)
-          .end(function(err,data){
+          .end(function(err,resp){
+            var data = resp.body;
             expect(data.id).not.to.be.undefined();
             expect(data.name).to.equal("Jeff");
             done(err);
@@ -84,7 +84,8 @@ describe('user', function () {
           .post('/api/handshake')
           .send({id: user.id}) // Phone should send just the ID
           .expect(200)
-          .end(function(err,data){
+          .end(function(err,resp){
+            var data = resp.body;
             expect(data.id).not.to.be.undefined();
             expect(data.name).to.equal("Jose");// This is the user we created at the star
             done(err);
